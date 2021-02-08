1d91c772d9c08babb12c124cbc045a10646c096f
unknown <mmellado@mail.sfsu.edu>
2021-02-08 04:34:15 -0800

    Added a template for the block chain

--- GatorCoin/blockchain.py
+++ GatorCoin/blockchain.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+# contains a linked list of blocks
+class Blockchain (object):
+    def __init__(self):
+        
+
+
+
+# contains transation, hash, and previous hash
+# index and time
+class Block (object):
+    def __init__(self):
+
+
+
+
+# contains send reciever amount time hash
+class Transaction (object):
+    def __init__(self):
