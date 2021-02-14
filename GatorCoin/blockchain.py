c7f5101512b2067fec1a624c12f83b5b399c8a6b
Gina somara <ginasomara@Ginas-MacBook-Pro.local>
2021-02-13 21:51:02 -0500

    Alfa build of GatorCoin

--- GatorCoin/blockchain.py
+++ GatorCoin/blockchain.py
@@ -1,18 +1,50 @@
-# contains a linked list of blocks
-class Blockchain (object):
-    def __init__(self):
-        
+from hashlib import sha256 as magic
+
+import datetime
 
 
+class BlockUtils:
+  @staticmethod
+  def hash(data):
+    return magic((datetime.datetime.now().strftime("%H:%M:%S")).encode('utf-8')).hexdigest()
 
-# contains transation, hash, and previous hash
-# index and time
-class Block (object):
-    def __init__(self):
+class Blockchain(object):
+    def __init__(self, val, next=None):
+        self.block = val
+        self.next = next
+
+# block object is list
+class Block(list):
+    def __init__(self, previousHash):
+        self.hash = BlockUtils.hash(previousHash) 
+        self.previous = previousHash
+        pass
+    
+    def addTransaction(self,transaction):
+        self.append(transaction)
+
+# transaction object
+class Transaction(object):
+    def __init__(self, sender, reciever, amount):
+        self.sender    = sender
+        self.reciever  = reciever
+        self.amount    = amount
+        self.transTime = datetime.datetime.now()
+        self.id        = magic((sender+reciever).encode('utf-8')).hexdigest() 
 
 
+# Test Run 
+if __name__ == '__main__':
+  genBlock = Block(0)
+  t1 = Transaction("Sender", "Reciever", 300)
+  t2 = Transaction("Jade", "Miguel", 900)
+  t3 = Transaction("Dustin", "Zach", 1500)
+  genBlock.addTransaction(t1)
+  genBlock.addTransaction(t2)
+  genBlock.addTransaction(t3)
+  blockchain = Blockchain(genBlock)
+  
+  for b in blockchain:
+    print(b)
 
 
-# contains send reciever amount time hash
-class Transaction (object):
-    def __init__(self):
