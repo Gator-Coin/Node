f6f6f2ee37af4a2edf63ebf13b79b944a1e6a2f9
Stephen Sams <Stephen.sams@live.com>
2021-02-15 13:37:37 -0800

    Update blockchain.py

--- GatorCoin/blockchain.py
+++ GatorCoin/blockchain.py
@@ -7,56 +7,114 @@ from hashlib import sha256
 import json
 
 class Ledger(list):
-    def __init__(self):
+    def __init__(self, file_path = None):
+        """ 
+        Creates a new Ledger object that cotains 
+
+        :param filename: loads from a file names relative or absolute path
+        """
         self.pending_transactions = []
+        if file_path:
+            self.load(file_path)
+        else:
+            self = list()
 
     def add_transaction(self, sender, receiver, amount):
-    """ 
-    Adds a new transactions to the pending_transactions list. 
-  
-    The pending_transactions list are all the transactions on the
-    network that need to be packed into the next block. 
-  
-    Parameters: 
-    sender (key): public key of the sender
-    receiver(key): private key of the receiver
-    amount(int): Amount of curency being transacted 
-  
-    Returns: 
-    int : 0  
-  
-    """
+        """ 
+        Adds a new transactions to the pending_transactions list. 
+    
+        The pending_transactions list are all the transactions on the
+        network that need to be packed into the next block. 
+    
+        :param sender: public key of the sender
+        :param receiver: private key of the receiver
+        :param amount: Amount of curency being transacted 
+    
+        :returns: true if the transaction was added to the list else returns the error
+        """
         self.pending_transactions.append(
             Transaction(sender, receiver, amount))
 
     def new_block(self, proof):
+        """ 
+        TODO: Create a new_block method
+    
+        :param proof: proof needs to be a byte object that can be hashed with the proof from the last block. 
+    
+        :returns: true if the block was created to
+
+        :throws: Invalid proof
+        """
         pass
 
     def save(self, file_path):
+        """ 
+        Overwrite a file with the blockchain
+    
+        :param file_path: reletive or absoulte path of the file to overwrite with the blockchain
+    
+        :returns: true if the file was saved
+
+        :throws: IOError
+        """
         json.dump(self, open(file_path, 'w'))
 
+
     def load(self, file_path):
+        """ 
+        Loads a json file containing the blockchain
+    
+        :param file_path: reletive or absoulte path of the file to overwrite with the blockchain
+    
+        :returns: true if the file was saved
+
+        :throws: IOError
+        """
         json.load(self, open(file_path, 'r'))
 
+    def update(self, file_path):
+        """ 
+        TODO: Create a function that appends a file with blocks not yet added to file
+        its important to make this work incrementally so when the file grows large the
+        node isnt forced to store the entire blockchain in memory
+    
+        :param file_path: reletive or absoulte path of the file to overwrite with the blockchain
+    
+        :returns: true if the file was saved
+
+        :throws: IOError
+        """
+        pass
 
 class Block(dict):
+    """ 
+    Creates a new block object
+
+    :param filename: loads from a file names relative or absolute path
+    """
     def __init__(self, proof, transactions):
         self["proof"] = proof
         self["transactions"] = transactions
         self["timestamp"] = str(timestamp())
 
-    def json(self):
+    def __json__(self):
+        """ 
+        :returns: the json representation of the block in the correct format to work with the hasing function
+        """
         return json.dumps(self, indent=2, sort_keys=True)
 
     def hash(self):
-        return sha256(self.json().encode("utf8")).hexdigest()
+        """ 
+        :returns: a hexidecimal hash that is unique to the block
+        """
+        return sha256(self.__json__().encode("utf8")).hexdigest()
 
 
 class Transaction(dict):
     def __init__(self, sender, receiver, amount):
         self["sender"] = sender
         self["receiver"] = receiver
-        self["amount"] = amount
+        self["amount"] = int(amount)
         self["timestamp"] = str(timestamp())
 
 if __name__ == '__main__':
